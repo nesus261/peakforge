dce3faa1018ff84d1e129574debad8c1368f24e5
unknown <nesus261@gmail.com>
2024-08-27 21:18:59 +0200

    little design fixes

--- app/components/home/whyUs.tsx
+++ app/components/home/whyUs.tsx
@@ -9,27 +9,27 @@ export default function WhyUs() {
         </h1>
         <div className="m-5 mt-14 mb-96 md:mb-5">
           <div className="flex flex-row flex-wrap justify-center">
-            <div className="basis-80 bg-sky-900 rounded-2xl p-6 m-5 mx-7 transform transition-transform duration-500 sm:hover:scale-110 md:hover:scale-125 hover:-translate-x-12 hover:-translate-y-7">
+            <div className="basis-80 bg-sky-900 rounded-2xl p-6 m-5 mx-7 transform transition-transform duration-500 hover:scale-105 md:hover:scale-125 hover:-translate-x-5 md:hover:-translate-x-12 hover:-translate-y-7">
               <h1 className="text-3xl font-bold">Profesjonalizm</h1>
               <h4 className="mt-1 text-slate-300 text-xl">
                 Każdy projekt realizujemy z dbałością o najmniejsze szczegóły.
               </h4>
             </div>
-            <div className="basis-96 bg-sky-700 rounded-2xl p-5 m-5 mx-7 transform transition-transform duration-700 sm:hover:scale-110 md:hover:scale-125 hover:translate-x-12 hover:-translate-y-7">
+            <div className="basis-96 bg-sky-700 rounded-2xl p-5 m-5 mx-7 transform transition-transform duration-700 hover:scale-105 md:hover:scale-125 hover:translate-x-5 md:hover:translate-x-12 hover:-translate-y-7">
               <h1 className="text-3xl font-bold">Innowacyjność</h1>
               <h4 className="mt-1 text-slate-300 text-xl">
                 Zawsze jesteśmy na bieżąco z najnowszymi trendami i
                 technologiami.
               </h4>
             </div>
-            <div className="pt-6 basis-80 bg-cyan-900 rounded-2xl p-5 m-5 mx-7 transform transition-transform duration-700 sm:hover:scale-110 md:hover:scale-125 hover:-translate-x-12 hover:translate-y-7">
+            <div className="pt-6 basis-80 bg-cyan-900 rounded-2xl p-5 m-5 mx-7 transform transition-transform duration-700 hover:scale-105 md:hover:scale-125 hover:translate-x-5 md:hover:-translate-x-12 hover:translate-y-7">
               <h1 className="text-3xl font-bold">Elastyczność</h1>
               <h4 className="mt-1 text-slate-300 text-xl">
                 Dostosowujemy nasze rozwiązania do indywidualnych potrzeb
                 klientów.
               </h4>
             </div>
-            <div className="mb-10 basis-72 bg-blue-900 rounded-2xl p-5 m-5 mt-9 mx-7 transform transition-transform duration-500 sm:hover:scale-110 md:hover:scale-125 hover:translate-x-12 hover:translate-y-7">
+            <div className="mb-10 basis-72 bg-blue-900 rounded-2xl p-5 m-5 mt-9 mx-7 transform transition-transform duration-500 hover:scale-105 md:hover:scale-125 hover:translate-x-5 md:hover:translate-x-12 hover:translate-y-3 md:hover:translate-y-7">
               <div className="">
                 <h1 className="text-3xl font-bold mt-2">Pasja</h1>
                 <h4 className="mt-1 text-slate-300 text-xl">
